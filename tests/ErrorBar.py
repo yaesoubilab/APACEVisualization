cbe964d15e76cfc6ce3e95d097a9a0314d1df347
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-09-11 17:03:31 -0400

    error bars

---
+++ tests/ErrorBar.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = [1, 2]
+y = [3, 4]
+x_err_l = [0.5, 1]
+x_err_u = [1, 2]
+y_err_l = [1, 2]
+y_err_u = [3, 5]
+
+x_err = [x_err_l, x_err_u]
+y_err = [y_err_l, y_err_u]
+
+fig, axs = plt.subplots(1, 1)
+axs.plot(x, y, 'o')
+
+axs.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='none', color='k', linewidth=1)
+
+plt.show()
