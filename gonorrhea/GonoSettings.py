009c40a5f3f049cb3dc1880e2e3ab21761e3ec03
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-09-21 19:53:17 -0400

    gono settings

--- gonorrhea/GonoSettings.py
+++ gonorrhea/GonoSettings.py
@@ -1,18 +1,21 @@
 
-INDX = 2
+INDX = 3
 X_RANGE = (-70, 70)
 Y_RANGE = (-0.7, 1.9)
 
 # file names for scenario analysis
-SA_FILE_NAMES = ['csvfiles\SAAll_WithDR.csv', 'csvfiles\SAAll_NoDR.csv', 'csvfiles\SAAll_NoDR_Noise.csv']
+SA_FILE_NAMES = ['csvfiles\SAAll_WithDR.csv',
+                 'csvfiles\SAAll_NoDR.csv',
+                 'csvfiles\SAAll_NoDR_Noise100.csv',
+                 'csvfiles\SAAll_NoDR_Noise1000.csv']
 SELECTED_SA_FILE_NAME = SA_FILE_NAMES[INDX]
 
 # file names for parameter analysis
 POSTERIOR_FILE_NAMES \
-    = ['csvfiles\ParamSamplesWithDR.csv', 'csvfiles\ParamSamplesNoDR.csv', 'csvfiles\ParamSamplesNoDR_v2.csv']
+    = ['csvfiles\ParamSamplesWithDR.csv', 'csvfiles\ParamSamplesNoDR.csv', 'csvfiles\ParamSamplesNoDR_v2.csv','']
 SELECTED_POSTERIOR_FILE_NAME = POSTERIOR_FILE_NAMES[INDX]
 
 # file names for calibration
 CALIB_FILE_NAMES \
-    = ['csvfiles\TrajsWithDR.csv', 'csvfiles\TrajsNoDR.csv', 'csvfiles\TrajsNoDR_v2.csv']
+    = ['csvfiles\TrajsWithDR.csv', 'csvfiles\TrajsNoDR.csv', 'csvfiles\TrajsNoDR_v2.csv','']
 SELECTED_CALIB_FILE_NAME = CALIB_FILE_NAMES[INDX]
