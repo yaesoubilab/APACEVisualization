b81c92eb85b90070e44fbd7787d20be8b3a564ee
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-12 00:03:51 -0400

    revised poly regression

--- apace/ScenariosClasses.py
+++ apace/ScenariosClasses.py
@@ -1,13 +1,11 @@
 import numpy as np
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from apace import helpers
 import csv
+from enum import Enum
 import SimPy.EconEvalClasses as Econ
 import SimPy.StatisticalClasses as Stat
-from enum import Enum
-import apace.RegressionClasses as Reg
+import SimPy.RegressionClasses as Reg
 
 
 ALPHA = 0.05    # confidence level
