c5e7f4bd0fb48549217d78e8e38a9df1f277b3a8
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-23 16:59:50 -0400

    updating the optimization visualizer

--- gonorrhea/GonoOptimization.py
+++ gonorrhea/GonoOptimization.py
@@ -6,9 +6,9 @@ IO.delete_files('.png', '../gonorrhea/figures/optimization_figs/')
 
 Vis.plot_all_opt_itrs(
     csv_directory='../gonorrhea/csvfiles/optimcsvfiles/',
-    n_vars=4,
+    n_vars=2,
     save_plots_directory='../gonorrhea/figures/optimization_figs/',
-    f_range=[0, 5*10e8],
-    x_ranges=[[0, 0.5], [-0.5, 0], [0, 0.5], [-0.5, 0]],
+    f_range=[-10e6, 10e6],
+    x_ranges=[[0, 0.5], [-0.5, 0.5], [0, 0.5], [-0.5, 0]],
     y_axis_labels=[r'$f(\tau, \theta)$', r'$\tau_1$', r'$\tau_2$', r'$\theta_1$', r'$\theta_2$']
 )
