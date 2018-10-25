3e6fc5de04eede44c9805c07b92f529312786f03
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-25 15:19:42 -0400

    new gono

--- gonorrhea/GonoOptimization.py
+++ gonorrhea/GonoOptimization.py
@@ -6,9 +6,9 @@ IO.delete_files('.png', '../gonorrhea/figures/optimization_figs/')
 
 Vis.plot_all_opt_itrs(
     csv_directory='../gonorrhea/csvfiles/optimcsvfiles/',
-    n_vars=2,
+    n_vars=4,
     save_plots_directory='../gonorrhea/figures/optimization_figs/',
     f_range=[-10e6, 10e6],
-    x_ranges=[[0, 0.5], [-0.5, 0.5], [0, 0.5], [-0.5, 0]],
+    x_ranges=[[0, 0.5], [-0.5, 0.5], [0, 1], [-0.25, 0.25]],
     y_axis_labels=[r'$f(\tau, \theta)$', r'$\tau_1$', r'$\tau_2$', r'$\theta_1$', r'$\theta_2$']
 )
