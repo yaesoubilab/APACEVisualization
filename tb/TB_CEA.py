fddc76551ed5a6c3a770783800cc219de5f77299
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-01-24 12:11:26 -0500

    TB new SA

--- tb/TB_CEA.py
+++ tb/TB_CEA.py
@@ -6,7 +6,7 @@ markers = ['o', 's', '^', 'D']
 colors = ['r', 'b', 'g', '#FF9912']
 
 PROB_UPTAKE = 0.75      # 0.5, 0.75, 1
-PROB_DROPOUT = 0.15     # 0.1, 0.15, 0.25,
+PROB_DROPOUT = 0.25     # 0.1, 0.15, 0.25,
 
 # conditions of variables to define scenarios to display on the cost-effectiveness plane
 # here we want scenarios with
