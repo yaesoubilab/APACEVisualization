0a4d1f8d679aa284261cb4b0d9d70606daa5d87d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-03-09 12:36:32 -0500

    partial rank

--- tb/PartialRankCorrelation.py
+++ tb/PartialRankCorrelation.py
@@ -18,10 +18,10 @@ parameter_values = IO.read_csv_cols_to_dictionary(
 prc = PRC.PartialRankCorrelation(parameter_values=parameter_values,
                                  output_values=daly_cost[0])
 
-prc.export_to_csv(file_name='PRC-DALY.csv',decimal=4)
+prc.export_to_csv(file_name='PRC-DALY.csv', decimal=4)
 
 # partial rank correlation for COST
 prc = PRC.PartialRankCorrelation(parameter_values=parameter_values,
                                  output_values=daly_cost[1])
 
-prc.export_to_csv(file_name='PRC-Cost.csv',decimal=4)
+prc.export_to_csv(file_name='PRC-Cost.csv', decimal=4)
