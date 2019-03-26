f6d30963d6c816c404d713b2ca407bccb4b8abc2
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-03-26 15:40:20 -0400

    param analysis

--- gonorrhea/PlotStepSizes.py
+++ gonorrhea/PlotStepSizes.py
@@ -1,7 +1,7 @@
 import SimPy.Optimization as opt
 
 
-opt.plot_step_size(a0s=[0.1],
-                   bs=[10, 25, 50, 75],
+opt.plot_step_size(a0s=[0.1, 0.05],
+                   bs=[10, 25, 50],
                    c0s=[0.05],
                    nItrs=1000)
