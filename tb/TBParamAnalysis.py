d816690ab93b217d4cd4e5701f881917049ab1c4
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-12 17:00:02 -0500

    updating priors

--- tb/TBParamAnalysis.py
+++ tb/TBParamAnalysis.py
@@ -7,16 +7,19 @@ paramDict = Param.Parameters('csv_files\SampledParams.csv')
 paramDict.calculate_means_and_intervals(poster_file='Posteriors.csv')
 
 # parameters ID
-ids=[2, 3, 4, 7, 8, 9, 77, 78]
-ids.extend(range(85, 92+1))
-ids.extend([129, 137, 145, 161])    # progression probability
-ids.extend([163, 164, 165])         # reactivation rate (HIV-)
-ids.extend([170, 171, 172])         # reactivation rate (HIV+nIC)
-ids.extend([177, 178, 179])         # reactivation rate (HIV+IC)
-ids.extend(range(229, 232))         # time until detection
-ids.extend(range(236, 239))
-ids.extend(range(243, 246))
-ids.extend(range(250, 253))
+ids=[2, 3, 4]                       # TB infectivity
+ids.extend([7, 8, 9])               # HIV infectivity
+ids.extend([95])                    # progression to HIV | IC
+ids.extend([96])                    # ration of dead rate exp to naive
+ids.extend(range(103, 103+8))       # partial immunity
+ids.extend([147, 155, 163, 179])    # progression probability
+ids.extend(range(181, 181+3))         # reactivation rate (HIV-)
+ids.extend(range(188, 188+3))         # reactivation rate (HIV+nIC)
+ids.extend(range(195, 195+3))         # reactivation rate (HIV+IC)
+ids.extend(range(247, 247+3))             # time until detection
+ids.extend(range(254, 254+3))
+ids.extend(range(261, 261+3))
+ids.extend(range(268, 268+3))
 
 paramDict.plot_histograms(ids=ids,
                           csv_file_name_prior='csv_files\ParamPriorDists.csv',
