8b2a1ae62efe9104c082ce388922ef9902d2912d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-02-20 16:49:52 -0500

    effective sample size

--- tb/TBEffSampleSize.py
+++ tb/TBEffSampleSize.py
@@ -6,6 +6,7 @@ data = IO.read_csv_cols(file_name='csv_files/TBLikelihoods.csv', n_cols=1,
                         if_ignore_first_row=True, if_convert_float=True)
 
 P.plot_eff_sample_size(likelihood_weights=data[0], if_randomize=True,
-                       y_range=(0, 10))
+                       y_range=(0, 10),
+                       file_name='results/EffSampleSize.png')
 
 
