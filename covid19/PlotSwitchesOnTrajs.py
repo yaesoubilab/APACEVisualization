4f971ae5d0f4bbabfd083da26e6fad3f9c7032b3
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-04-13 22:15:39 -0400

    added number of switches

--- covid19/PlotSwitchesOnTrajs.py
+++ covid19/PlotSwitchesOnTrajs.py
@@ -2,6 +2,8 @@ from apace import TrajectoriesClasses as Vis
 import matplotlib.pyplot as plt
 import os
 
+ID_TO_DISPLAY = 2
+
 
 def get_dfs(csv_file_names):
     dfs = []
@@ -17,30 +19,37 @@ def get_dfs(csv_file_names):
             warmup_epi_time=1))
     return dfs
 
+
 titles =['Social distancing never employed',
          'Adaptive policy corresponding to the willingness-to-pay of $100,000',
+         'Adaptive policy corresponding to the willingness-to-pay of $150,000',
          'Adaptive policy corresponding to the willingness-to-pay of $200,000',]
 
-dfs = get_dfs(csv_file_names=['No.csv', 'WTP100.csv', 'WTP200.csv'])
+dfs = get_dfs(csv_file_names=['No.csv', 'WTP100.csv', 'WTP150.csv', 'WTP200.csv'])
 
 os.chdir('..')
-fig, axes = plt.subplots(3, 1, figsize=(7.5, 7)) #sharex=True)
+fig, axes = plt.subplots(len(titles), 1, figsize=(7.5, 9)) #sharex=True)
 
 for i, df in enumerate(dfs):
+
     df.add_to_ax(
         ax=axes[i],
         plot_info=Vis.PlotTrajInfo(
             traj_name='Obs: To I',
-            y_range=[0, 20],
+            y_range=[0, 25],
             y_multiplier=0.001,
             x_label='Week',
             x_range=[0, 52],
-            y_label='Weekly Number of Cases\n(Thousands)',
+            y_label='Weekly Cases\n(Thousands)',
             title=titles[i],
             common_color_code='blue',
             transparency=1),
-        trajs_ids_to_display=[0]
+        trajs_ids_to_display=[ID_TO_DISPLAY]
     )
+    for t, action in enumerate(df.allTrajs['Obs: Action Code'].trajs[ID_TO_DISPLAY].obss):
+        if action == '1,1':
+            axes[i].bar(t, 10, color='red', alpha=0.25)
+
 
 fig.tight_layout()
 fig.savefig('covid19/figures/SwitchesOnTrajs.png', dpi=300, bbox_inches='tight')
