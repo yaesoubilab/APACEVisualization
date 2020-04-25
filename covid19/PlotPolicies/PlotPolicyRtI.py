550f47bda278d7b0c4e8ab184ce04e9af3216f3a
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-04-25 09:21:53 -0400

    opt fig

--- covid19/PlotPolicies/PlotPolicyRtI.py
+++ covid19/PlotPolicies/PlotPolicyRtI.py
@@ -4,7 +4,7 @@ import covid19.PolicyClasses as P
 import os
 
 # ---- settings ----
-POLICY_PARAMS = [1.09924, -0.11247, 0.19871, 0.0214, -0.08848, 0.5]#
+POLICY_PARAMS = [1.5, -0.1, 0.2, 0.05, -0.1, 1]#
 WTPS = np.linspace(0.25e5, 0.75e5, 9)  # [min, max, number of points]
 
 # change the current working directory
