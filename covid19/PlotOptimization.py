5d5875b815d5a5f44b407d0cb26f81fe77e718a3
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-04-24 22:46:12 -0400

    opt fig

--- covid19/PlotOptimization.py
+++ covid19/PlotOptimization.py
@@ -6,11 +6,11 @@ IO.delete_files('.png', 'figures/optimization_figs/')
 
 Vis.plot_all_opt_itrs(
     csv_directory='csv_files/optimization_csvs/',
-    n_vars=3,
+    n_vars=6,
     save_plots_directory='figures/optimization_figs/',
     show_titles=False,
     #f_range=[-1e6, 1e6],
-    var_ranges=[[0, 10], [-2, 0], [0, 1], [-0.000025, 0]],
-    y_axis_labels=[r'$f$', r'$\tau_0$', r'$\tau_1$', r'$\rho_0$', r'$\rho_1$'], # , r'$\rho_1$'
+    var_ranges=[[0, 4], [-1, 0], [0, 0.5], [0, 1], [-1, 0], [0, 1]],
+    y_axis_labels=[r'$f$', r'$\tau_0$', r'$\tau_1$', r'$\rho_0$', r'$\rho_1$', '', ''], # , r'$\rho_1$'
     window=20
 )
