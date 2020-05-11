250f672472c7c54fd8433d80239e97fcb593b743
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-05-11 19:02:33 -0400

    policy i

--- covid19/PlotPolicies/PlotPolicyIWithFuncForm.py
+++ covid19/PlotPolicies/PlotPolicyIWithFuncForm.py
@@ -4,9 +4,9 @@ import covid19.PolicyClasses as P
 import os
 
 # ---- settings ----
-POLICY_PARAMS = [4.18376,-1.67256,3.62718,-1.20251]# not bad: [7, -1, 7, -1]
+POLICY_PARAMS = [6.69172,-2.82836,6.23949,-2.60692]# not bad: [7, -1, 7, -1]
 # good option: [3.5, -0.5, 0.25, 500, -0.5, 0.5] for WTPS = np.linspace(1e5, 3e5, 9)
-WTPS = np.linspace(0.75, 1.25, 9)  # [min, max, number of points]
+WTPS = np.linspace(0.25, 1.25, 9)  # [min, max, number of points]
 
 # change the current working directory
 os.chdir('../..')
