ee3138c9c022fe13b61b2a3b732cb12916426596
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-05-11 22:12:51 -0400

    policy i

--- covid19/PlotPolicies/PlotPolicyIWithFuncForm.py
+++ covid19/PlotPolicies/PlotPolicyIWithFuncForm.py
@@ -4,7 +4,7 @@ import covid19.PolicyClasses as P
 import os
 
 # ---- settings ----
-POLICY_PARAMS = [6.69172,-2.82836,6.23949,-2.60692]# not bad: [7, -1, 7, -1]
+POLICY_PARAMS = [7, -1, 7, -1]# not bad: [7, -1, 7, -1]
 # good option: [3.5, -0.5, 0.25, 500, -0.5, 0.5] for WTPS = np.linspace(1e5, 3e5, 9)
 WTPS = np.linspace(0.25, 1.25, 9)  # [min, max, number of points]
 
